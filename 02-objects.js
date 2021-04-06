3a4e8f9b119e97d280a2cd5db961a8c914014542
JM <wkdwoals96@gmail.com>
2021-04-06 09:47:21 +0900

    object

---
+++ 02-objects.js
@@ -0,0 +1,61 @@
+// 속성
+// 프로퍼티 : 객체가 가진 데이터
+// 메서드 : 객제가 가진 기능함수 
+
+// 객체 생성방법1
+const person = new Object();
+
+person.name = "홍길동"
+person.age = 32;
+
+person.showInfo = function(){
+    let message = `${this.name}, ${this.age}`;
+    console.log(message);
+}
+console.log("person",person);
+
+// 메서드 호출
+person.showInfo();
+
+// 객체 생성방법2 : JSON
+// {} - >  객체 표기 , 속성들은 KEY:VALUE 쌍으로 기술
+console.log("------------------------------------")
+const person2 = {
+    name: '장길산',
+    age : 40,
+    showInfo: function(){
+        let message = `${this.name}, ${this.age}`;
+        console.log(message);
+    }
+
+}
+console.log("person2:",person2);
+person2.showInfo();
+
+// 객체 생성방법3 : 함수를 생성자 처럼 활용
+console.log("-----------------------------------------")
+const Member = function(name, position){
+    this.name = name;
+    this.position = position;
+}
+console.log(Member);
+let m1 = new Member("강백호","pf");
+let m2 = new Member("서태웅","sf");
+
+console.log(m1, m2);
+
+// 동적으로 메서드 추가 가능
+m1.sayHi = function(){
+    console.log(`${this.name},${this.position}`);
+}
+m1.sayHi();
+
+console.log("m1의 생성객체: ", m1.constructor); // m1을 생성한 생성 객체 확인
+// 동일 객체는 prototype 이라는 공유 영역을 가지고 있다
+// 이 객체를 이용 ,인스턴스화한 모든 인스턴스에 동일한 속성 공유 가능
+console.log("Member 객체의 prototype 영역: ",Member.prototype);
+Member.prototype.introduce = function(){
+    console.log("name :" + this.name);
+    console.log("position :" + this.position);
+}
+console.log(Member.prototype);
--- basic.js
+++ basic.js
@@ -17,13 +17,26 @@ console.log(n1, n2);
 console.log(typeof n1, typeof n2);
 // is Nan 
 console.log(isNaN(n1));
+
 // String 문자열 추출 및 검색
 // charAt(index)  : 특정 인덱스의 문자를 추출
 // substr(from, length) : 문자열내의 from 부터 lengh길이만큼 추출
 // substring(from , end): 문자열내의 from 부터 end 인덱스까지의 문자열출
 
 const s = "THIS IS JAVASCRIPT"
-console.log(s.charAt(8)); // 7번인덱스 추출
-console.log(s[8]);
+console.log(s.charAt(8)); // 8번인덱스 추출
+console.log(s[8]); // 8번인덱스 추출
 console.log(s.substr(1, 3)); // 1부터 5개 추출
 console.log(s.substring(1, 3)); // 1부터 5번까지 추출
+
+// replace , trim , indexOf .....
+
+// ES6방식
+temp = 20;
+message = `현재 온도는
+              ${temp}입니다`; 
+console.log(message);
+
+// 선언은 했으나 할당되지 않은 변수 -> undefined 
+let v1; // undefined
+let v2 = undefined;
